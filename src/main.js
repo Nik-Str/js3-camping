bb239a8770fd2774a38461e95a75b1cf39dd0958
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-05-18 12:08:15 +0200

    activities page first draft

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
+import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(VueGoogleMaps, {
+    load: {
+      key: process.env.VUE_APP_MAPS,
+    },
+  })
+  .mount('#app');
---
+++ src/views/Activities.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="backgroundImg">
+    <div class="d-flex justify-center text-white text-center align-center" :style="{ height: '35vh' }">
+      <div>
+        <h1 class="text-h3 font-weight-bold">AKTIVITETER FÖR HELA FAMILJEN</h1>
+        <h3 class="text-h6 font-weight-bold font-italic">Sola, bada och välj bland flera olika aktiviteter</h3>
+      </div>
+    </div>
+  </div>
+
+  <v-container fluid>
+    <ActivitiesCard class="mb-3" :data="activities" />
+    <div class="elevation-1 rounded-lg closeBy mb-4">
+      <div class="closeByOverlayer d-flex justify-center align-center text-center">
+        <h2 class="text-h4 text-white font-weight-bold">NÄRLIGGANDE BADPLATSER</h2>
+      </div>
+    </div>
+    <Maps />
+  </v-container>
+</template>
+
+<script>
+import Maps from '../components/Maps.vue';
+import ActivitiesCard from '../components/ActivitiesCard.vue';
+
+export default {
+  name: 'Activities',
+  data() {
+    return {
+      activities: [
+        { title: 'Mat & Dryck', sub: 'Bred variation från väldens alla hörn', img: require('@/assets//food1.jpg') },
+      ],
+    };
+  },
+  components: {
+    Maps,
+    ActivitiesCard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.backgroundImg {
+  background-image: url('../assets/activities_bg.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.closeBy {
+  background-image: url('../assets/activities_sm.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 7rem;
+  width: 100%;
+}
+
+.closeByOverlayer {
+  background-color: rgba(250, 250, 250, 0.503);
+  width: 100%;
+  height: 100%;
+}
+
+.v-container {
+  max-width: 60vw;
+}
+
+@media only screen and (max-width: 960px) {
+  .v-container {
+    max-width: 100vw;
+  }
+}
+</style>
