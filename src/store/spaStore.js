5f8dc46ef91e1f3d10e8d35d442f0a1b583062b2
Zenice90 <theohenriksson90@gmail.com>
2022-05-20 08:35:36 +0200

    test
    test

--- src/store/spaStore.js
+++ src/store/spaStore.js
@@ -6,3 +6,4 @@ const store = {
 };
 
 export default store;
+
---
+++ src/views/SpaView.vue
@@ -0,0 +1,19 @@
+<template>
+<v-img src="https://media.happy-day.se//happyday/images/massage-2018-12-11-144703801/0/0/0/massage.jpg"></v-img>
+<v-card-title>Massage</v-card-title>
+<v-card-text>
+    <v-rating
+        empty-icon="$mdiStarOutline"
+        full-icon="$mdiStar"
+        half-icon="$mdiStarHalfFull"
+        hover
+        length="5"
+        size="64"
+        value="4"
+    ></v-rating>
+</v-card-text>
+</template>
+
+<script>
+
+</script>
